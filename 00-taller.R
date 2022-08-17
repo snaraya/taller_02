8e4debb030e4ff0500ceb1411e79d78097fdaa22
Soledad <snaraya@uc.cl>
2022-08-16 23:02:25 -0400

    actualización distill

---
+++ 00-taller.R
@@ -0,0 +1,17 @@
+# Semana 2: Taller
+
+#### Script y R Markdown ####
+
+# Utilizar un script es útil para tener soluciones rápidas a nuestros problemas. 
+# Pero es un elemento que tiene muchas limitaciones. Por esa razón, en la actualidad,
+# para el desarrollo de reportes o scripts que requieran de explicaciones para quienes
+# los usan --principalmente pensando en la replicabilidad de lo que estamos estudiando--
+# se ha generado una nueva forma de presentar este tipo de archivo.
+
+# Con este objetivo en mente, se ha desarrollado las plantillas "R Markdown" que nos
+# permiten no sólo generar un script más ordenado y con más opciones, sino también
+# todo tipo de reportes en diferentes formatos (html, word, pdf, etc.).
+
+# Para eso, instalaremos:
+
+install.packages("rmarkdown")
